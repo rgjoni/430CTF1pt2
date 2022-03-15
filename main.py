a3ddc2d3dc563c0acf3507f7b5240554742d5cd8
audreybongalon <bongalon@usc.edu>
2022-03-15 13:24:12 -0700

    added comments, cleaned up formatting

--- main.py
+++ main.py
@@ -1,115 +1,154 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
+# importing flask module in the project is mandatory
+# an object of flask class is our wsgi application
 from flask import Flask, request, render_template, redirect, g, make_response
 import sqlite3
 from passlib.hash import sha256_crypt
+
+
+
 
 hostname = "localhost"
 port = 5001
 address = "http://" + hostname + ":" + str(port)
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+# start the flask app. the name of this program is passed to the constructor
 app = Flask(__name__)
 
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+
+
+# base page (if no path is specified. this is just a backup)
+@app.route("/")
+# "/" URL is bound with hello_world() function.
 def base():
-    return 'Base page'
+    return "base page\n"
 
-@app.route('/blue', methods=['GET'])
+
+
+
+# similar to the base page above. this should not be called
+@app.route("/blue", methods=["GET"])
 def handle_main():
-    return 'Main Page'
+    return "main page\n"
 
 
 
-    
-@app.route('/blue/register', methods=['GET'])
+
+# register a new user
+@app.route("/blue/register", methods=["GET"])
 def handle_register():
+    # get username and password
     user = request.args.get("user")
     password = request.args.get("pass")
     password = sha256_crypt.hash(password)
-    users = conn.execute('SELECT balance AS id, * FROM users WHERE username = ?;', (user,)).fetchone()
+
+    # check for duplicates
+    users = conn.execute("SELECT balance AS id, * FROM users WHERE username = ?;", (user,)).fetchone()
     if users is not None:
-        return "error duplicate entry"
-    conn.execute('INSERT INTO users (username, password, balance) VALUES (?, ?, ?)', (user, password, 0))
+        return "error: duplicate entry\n"
+
+    # create a new user
+    conn.execute("INSERT INTO users (username, password, balance) VALUES (?, ?, ?)", (user, password, 0))
     conn.commit()
     resp = make_response()
     return resp
 
 
-@app.route('/blue/login', methods=['GET'])
+
+
+# log in as an existing user
+@app.route("/blue/login", methods=["GET"])
 def handle_login():
+    # get username and password
     user = request.args.get("user")
     password = request.args.get("pass")
-    users = conn.execute('SELECT password, * FROM users WHERE username = ?;', (user,)).fetchone()
+    users = conn.execute("SELECT password, * FROM users WHERE username = ?;", (user,)).fetchone()
+
     if users is None:
-        return ""
-    if sha256_crypt.verify(password, users[0]):
+        return "user not found\n"
+
+    # attempt to log in the user
+    if sha256_crypt.verify(password, users[0]):     # valid user
         resp = make_response()
-        resp.set_cookie('username', user)
+        resp.set_cookie("username", user)
         return resp
-    else:
+    else:                                           # invalid user
         resp = make_response()
         return resp
 
 
-@app.route('/blue/manage', methods=['GET'])
+
+
+# do transactions and whatnot
+@app.route("/blue/manage", methods=["GET"])
 def action_handler():
     action = request.args.get("action")
     amount = request.args.get("amount")
-    if(action == "deposit"):
-        print(request.cookies.get('username'))
-        users = conn.execute('SELECT balance AS id, * FROM users WHERE username = ?;', (request.cookies.get('username'),)).fetchone()
+
+    # add funds
+    if action == "deposit":
+        print(request.cookies.get("username"))
+        users = conn.execute("SELECT balance AS id, * FROM users WHERE username = ?;", (request.cookies.get("username"),)).fetchone()
         if users is None:
             return "err"
-        newAmount = users[0]
-        newAmount = newAmount + int(amount)
-        conn.execute('UPDATE users SET balance = ? WHERE username = ?;', (newAmount, request.cookies.get('username')))
+        new_amount = users[0]
+        new_amount = new_amount + int(amount)
+        conn.execute("UPDATE users SET balance = ? WHERE username = ?;", (new_amount, request.cookies.get("username")))
         conn.commit()
-        return str(newAmount)
-    elif(action == "withdraw"):
-        users = conn.execute('SELECT balance AS id, * FROM users WHERE username = ?;', (request.cookies.get('username'),)).fetchone()
+        return str(new_amount)
+
+    # withdraw funds
+    elif action == "withdraw":
+        users = conn.execute("SELECT balance AS id, * FROM users WHERE username = ?;", (request.cookies.get("username"),)).fetchone()
         if users is None:
             return "err"
-        newAmount = users[0]
-        newAmount = newAmount - int(amount)
-        conn.execute('UPDATE users SET balance = ? WHERE username = ?;', (newAmount, request.cookies.get('username')))
+        new_amount = users[0]
+        new_amount = new_amount - int(amount)
+        conn.execute("UPDATE users SET balance = ? WHERE username = ?;", (new_amount, request.cookies.get("username")))
         conn.commit()
-        return str(newAmount)
-    elif (action == "balance"):
-        users = conn.execute('SELECT balance AS id, * FROM users WHERE username = ?;', (request.cookies.get('username'),)).fetchone()
+        return str(new_amount)
+
+    # check the current balance
+    elif action == "balance":
+        users = conn.execute("SELECT balance AS id, * FROM users WHERE username = ?;", (request.cookies.get("username"),)).fetchone()
         if users is None:
             return "err"
-        newAmount = users[0]
-        print(newAmount)
-        return str(newAmount)
-    elif (action == "close"):
-        conn.execute('DELETE FROM users WHERE username = ?', (request.cookies.get('username'),))
+        new_amount = users[0]
+        print(new_amount)
+        return str(new_amount)
+
+    # close the account (delete the user from the database)
+    elif action == "close":
+        conn.execute("DELETE FROM users WHERE username = ?", (request.cookies.get("username"),))
         conn.commit()
         resp = make_response()
-        resp.set_cookie('username', '', expires=0)
+        resp.set_cookie("username", "", expires=0)
         return resp
 
-@app.route('/blue/logout', methods=['GET'])
+
+
+
+# let the user log out
+@app.route("/blue/logout", methods=["GET"])
 def handle_logout():
     resp = make_response()
-    resp.set_cookie('username', '', expires=0)
+    resp.set_cookie("username", "", expires=0)
     return resp
 
+
+
+
+
+
+
 # main driver function
-if __name__ == '__main__':
-    # run() method of Flask class runs the application
-    # on the local development server.
-    conn = sqlite3.connect('database.db', check_same_thread=False)
-    with open('database.sql') as f:
+if __name__ == "__main__":
+    # connect to the database
+    conn = sqlite3.connect("database.db", check_same_thread=False)
+    with open("database.sql") as f:
         conn.executescript(f.read())
 
     cur = conn.cursor()
     conn.commit()
-    
+
     app.run(host=hostname, port=port)
